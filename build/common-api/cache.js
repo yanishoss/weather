ff2163f2cf3413fb65f240a2d6a728199a5e989c
Yanis HEDJEM <yanishproduction@gmail.com>
2018-07-05 16:57:49 +0200

    refactor(cache.ts): add a lock on the cache file
    Add the "proper-lockfile" dependency to lock the cache file

--- build/common-api/cache.js
+++ build/common-api/cache.js
@@ -4,28 +4,51 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
+const proper_lockfile_1 = require("proper-lockfile");
 const user_home_1 = __importDefault(require("user-home"));
 const PATH = `${user_home_1.default}/weather.lock`;
 class Cache {
     constructor() {
         this.lockfile = {};
-        fs_1.readFile(PATH, 'utf8', (err, lockfile) => {
-            if (err) {
-                return;
-            }
-            this.lockfile = JSON.parse(lockfile);
-        });
+        try {
+            proper_lockfile_1.lock(PATH, {
+                retries: 1000
+            })
+                .then((release) => {
+                fs_1.readFile(PATH, 'utf8', (err, lockfile) => {
+                    if (err) {
+                        return;
+                    }
+                    this.lockfile = JSON.parse(lockfile);
+                    release();
+                });
+            });
+        }
+        catch (e) {
+            throw new Error(`There is a problem with the cache file locking. Here is the error: ${e}`);
+        }
     }
     fetch(key) {
         return this.lockfile[key];
     }
     push(key, data) {
         this.lockfile[key] = data;
-        fs_1.writeFile(PATH, JSON.stringify(this.lockfile), (err) => {
-            if (err) {
-                throw err;
-            }
-        });
+        try {
+            proper_lockfile_1.lock(PATH, {
+                retries: 1000
+            })
+                .then((release) => {
+                fs_1.writeFile(PATH, JSON.stringify(this.lockfile), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    release();
+                });
+            });
+        }
+        catch (e) {
+            throw new Error(`There is a problem with the cache file locking. Here is the error: ${e}`);
+        }
     }
 }
 exports.Cache = Cache;
--- src/common-api/cache.ts
+++ src/common-api/cache.ts
@@ -1,4 +1,5 @@
 import { readFile, writeFile } from 'fs';
+import { lock } from 'proper-lockfile';
 import userHome from 'user-home';
 
 const PATH = `${userHome}/weather.lock`;
@@ -6,12 +7,21 @@ const PATH = `${userHome}/weather.lock`;
 export class Cache {
   private lockfile: {[name: string]: any} = {};
   constructor() {
-    readFile(PATH, 'utf8', (err, lockfile) => {
-      if (err) {
-        return;
-      }
-      this.lockfile = JSON.parse(lockfile);
-    });
+    try {
+      lock(PATH, { retries: 1000 })
+          .then((release) => {
+            readFile(PATH, 'utf8', (err, lockfile) => {
+              if (err) {
+                return;
+              }
+              this.lockfile = JSON.parse(lockfile);
+              release();
+            });
+          },
+        );
+    } catch (e) {
+      throw new Error(`There is a problem with the cache file locking. Here is the error: ${e}`);
+    }
   }
 
   public fetch<T>(key: string): T | undefined {
@@ -21,10 +31,20 @@ export class Cache {
   public push(key: string, data: any): void {
     this.lockfile[key] = data;
 
-    writeFile(PATH, JSON.stringify(this.lockfile), (err) => {
-      if (err) {
-        throw err;
-      }
-    });
+    try {
+      lock(PATH, { retries: 1000 })
+        .then((release) => {
+          writeFile(PATH, JSON.stringify(this.lockfile), (err) => {
+            if (err) {
+              throw err;
+            }
+
+            release();
+          });
+        },
+        );
+    } catch (e) {
+      throw new Error(`There is a problem with the cache file locking. Here is the error: ${e}`);
+    }
   }
 }
